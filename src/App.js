cb45e37217b033aa43d488bbc5bd05c80c0c5b0e
valfra0425 <valmirfrancisco1@outlook.com>
2024-04-17 15:56:19 -0300

    mudei a forma que o tabuleiro é constuido

--- src/App.js
+++ src/App.js
@@ -53,24 +53,25 @@ function Board({ xIsNext, squares, onPlay }) {
   }
 
   return (
+    // Nessa parte do código estou utilizando 2 interações para desenhar o tabuleiro
+    // Importante se atentar ao uso de () quando for escrever html e o {} quando for escrever instruções de código
     <>
-      <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={ squares[0]} onSquareClick={() => handleClick(0)}/>
-        <Square value={ squares[1]} onSquareClick={() => handleClick(1)}/>
-        <Square value={ squares[2]} onSquareClick={() => handleClick(2)}/>
+    <div className="status">{status}</div>
+    {[0, 1, 2].map(row => (
+      <div className="board-row" key={row}>
+        {[0, 1, 2].map(col => {
+          const squareIndex = row * 3 + col;
+          return (
+            <Square
+              key={squareIndex}
+              value={squares[squareIndex]}
+              onSquareClick={() => handleClick(squareIndex)}
+            />
+          );
+        })}
       </div>
-      <div className="board-row">
-        <Square value={ squares[3]} onSquareClick={() => handleClick(3)}/>
-        <Square value={ squares[4]} onSquareClick={() => handleClick(4)}/>
-        <Square value={ squares[5]} onSquareClick={() => handleClick(5)}/>
-      </div>
-      <div className="board-row">
-        <Square value={ squares[6]} onSquareClick={() => handleClick(6)}/>
-        <Square value={ squares[7]} onSquareClick={() => handleClick(7)}/>
-        <Square value={ squares[8]} onSquareClick={() => handleClick(8)}/>
-      </div>
-    </>
+    ))}
+  </>
   );
 }
 
